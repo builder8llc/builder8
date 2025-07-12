76cbabf6d0b303d0670a4473d5a7f905ebbe60ac
builder8llc <builder8llc@gmail.com>
2025-07-12 14:16:09 +0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 Builder8 is Live!</h1>
+      <p>The world's smartest PaaS for launching SaaS apps from AI prompts.</p>
+    </div>
+  );
+}
+
+export default App;
